dba69779129bedd2fa9f3af77d5d57d0940fcd6c
SWEAmal43 <Amalalsharari77@gmail.com>
2025-04-16 00:48:31 +0300

    Added Heap Sort algorithm and its JUnit tests

--- src/sortingtechniquesproject/SortingTechniquesProject.java
+++ src/sortingtechniquesproject/SortingTechniquesProject.java
@@ -58,6 +58,12 @@ public class SortingTechniquesProject {
         mergeSort(mergeSortedNumbers);
         System.out.print("mergeSort: ");
         printArray(mergeSortedNumbers);
+        
+         int[] heapSortedNumbers = numbers.clone();
+        mergeSort(heapSortedNumbers);
+        System.out.print("heapSort: ");
+        printArray(heapSortedNumbers);
+        
     }
     
     public static void printArray(int[] arr) {
@@ -172,6 +178,44 @@ public static int[] merge(int[] left, int[] right, int[] arr) {
         quickSort(arr, pi + 1, high);
     }
     return arr;
+}
+    
+public static void heapSort(int[] array) {
+    int n = array.length;
+
+   
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(array, n, i);
+
+    
+    for (int i = n - 1; i > 0; i--) {
+        
+        int temp = array[0];
+        array[0] = array[i];
+        array[i] = temp;
+
+        
+        heapify(array, i, 0);
+    }
+}
+
+private static void heapify(int[] array, int n, int i) {
+    int largest = i; 
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && array[left] > array[largest])
+        largest = left;
+
+    if (right < n && array[right] > array[largest])
+        largest = right;
+
+    if (largest != i) {
+        int swap = array[i];
+        array[i] = array[largest];
+        array[largest] = swap;
+        heapify(array, n, largest);
+    }
 }
 
 private static int partition(int[] arr, int low, int high) {
