d082a767afab3f416aff5884c65aa5660349f32c
SWEAmal43 <Amalalsharari77@gmail.com>
2025-04-16 16:25:06 +0300

    [SCRUM-5] Added Heap Sort algorithm and its JUnit tests

--- src/sortingtechniquesproject/SortingTechniquesProject.java
+++ src/sortingtechniquesproject/SortingTechniquesProject.java
@@ -19,8 +19,7 @@ public class SortingTechniquesProject {
     }
 }
         
-        int[] numbers = new int[size];
-        
+        int[] numbers = new int[size];        
         System.out.println("input array elements:");
         for (int i = 0; i < size; i++) {
             System.out.print("element " + (i + 1) + ": ");
@@ -58,6 +57,11 @@ public class SortingTechniquesProject {
         mergeSort(mergeSortedNumbers);
         System.out.print("mergeSort: ");
         printArray(mergeSortedNumbers);
+        
+        int[] heapSortedNumbers = numbers.clone();
+        heapSort(heapSortedNumbers);
+        System.out.print("heapSort: ");
+        printArray(heapSortedNumbers);
     }
     
     public static void printArray(int[] arr) {
@@ -192,6 +196,37 @@ private static int partition(int[] arr, int low, int high) {
     arr[i + 1] = arr[high];
     arr[high] = temp;
     return i + 1;
+}
+public static void heapSort(int[] array) {
+    int n = array.length;
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(array, n, i);
+
+    for (int i = n - 1; i > 0; i--) {
+        int temp = array[0];
+        array[0] = array[i];
+        array[i] = temp;
+        heapify(array, i, 0);
+    }
+}
+
+private static void heapify(int[] array, int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && array[left] > array[largest])
+        largest = left;
+
+    if (right < n && array[right] > array[largest])
+        largest = right;
+
+    if (largest != i) {
+        int swap = array[i];
+        array[i] = array[largest];
+        array[largest] = swap;
+        heapify(array, n, largest);
+    }
 }}
 
 
